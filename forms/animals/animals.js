042ee89239c49ec1f90e39af4a4361ed02f453d2
dtn75570 <71202757+dtn75570@users.noreply.github.com>
2020-10-05 14:09:08 -0500

    I finished the first Form
    I wrote the codes and finished the Animals form. It should be working now.

---
+++ forms/animals/animals.js
@@ -0,0 +1,24 @@
+/*Created a new form named animals.
+    Created an array named myAnimals with these animals in it:
+       dog, cat, horse, meerkat
+    Got the name of an animal from the user and adds it to the end of the array using an array method. 
+    Included code so the user can use any mix of case to enter the animal name. 
+    eg. the user could enter:
+            eagle
+            Eagle
+            EAGLE
+            EaglE
+                etc.
+    Output the last animal in the array using an array method to an alert using a template literal to the console with the format below. Used the myAnimals array in the template literal to show the last animal added: 
+    eg. Say the last animal in the array is 'eagle'. Then the output would be: 
+           "The last animal is a/an eagle."
+    Code is all commented out. 
+    Program runs without error.
+*/
+var myAnimals = ["dog", "cat", "horse", "meerkat"]
+
+var inptAnimals = prompt("Please input the name of the animal.")
+var lowerCaseAnimals = inptAnimals.toLowerCase()
+myAnimals.push(lowerCaseAnimals)
+var outputAnimals = myAnimals[myAnimals.length -1]
+alert(`The last animal is a/an ${outputAnimals}`)
